a0362dcfa76cf53694a902de909883634e3598a4
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2020-10-19 12:34:28 +0300

    Updates

---
+++ precache-manifest.116c8460e2b1bae488aa07e7b4a2cb06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43dbb2709d51ecfad0c7d89cfa1821b1",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "ede560c59e8ee3491716",
+    "url": "/SocialNetwork/static/css/main.ea49e48a.chunk.css"
+  },
+  {
+    "revision": "51b16c9a766c32f79eeb",
+    "url": "/SocialNetwork/static/js/2.ea498d6d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/SocialNetwork/static/js/2.ea498d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ede560c59e8ee3491716",
+    "url": "/SocialNetwork/static/js/main.d95a13f1.chunk.js"
+  },
+  {
+    "revision": "cbcf78d674966714af32",
+    "url": "/SocialNetwork/static/js/runtime-main.bbb63f0e.js"
+  }
+]);
