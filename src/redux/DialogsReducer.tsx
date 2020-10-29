0986b8e08f9818d8e98c5727ed076291d0554d08
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2020-10-29 20:55:25 +0300

    change

--- src/redux/DialogsReducer.tsx
+++ src/redux/DialogsReducer.tsx
@@ -1,17 +1,17 @@
-import {StatePropsType, ActionTypes, DialogsPagePropsType} from "./State";
+// import {StatePropsType, ActionTypes, DialogsPagePropsType} from "./State";
 
-const SEND_MESSAGE = "SEND-MESSAGE"
-const CHANGE_MESSAGE = "CHANGE-MESSAGE"
+// const SEND_MESSAGE = "SEND-MESSAGE"
+// const CHANGE_MESSAGE = "CHANGE-MESSAGE"
 
-export const profileReducer = (state: StatePropsType, action: ActionTypes) => {
-    if (action.type === SEND_MESSAGE) {
-        let newMessage = { id: 6, textMessage: state.newMessageText };
-        state.messages.push(newMessage);
-        state.newMessageText = "";
+// export const profileReducer = (state: StatePropsType, action: ActionTypes) => {
+//     if (action.type === SEND_MESSAGE) {
+//         let newMessage = { id: 6, textMessage: state.newMessageText };
+//         state.messages.push(newMessage);
+//         state.newMessageText = "";
 
-    } else if (action.type === CHANGE_MESSAGE) {
-        state.newMessageText = action.messageText;
-    }
+//     } else if (action.type === CHANGE_MESSAGE) {
+//         state.newMessageText = action.messageText;
+//     }
 
-    return state;
-}
+//     return state;
+// }
