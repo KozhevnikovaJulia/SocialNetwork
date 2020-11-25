c8eafb951057da4856e6701b402aa2d554583260
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2020-11-25 14:26:18 +0300

    styles grey-black-pink

--- src/components/Profile/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./Profile.module.css";
 import {Preloader} from "../../common/Preloader/Preloader";
+import mainImg from "../../images/wZlmqJc56E1114.jpg"
 
 type ProfileInfoPropsType = {
   profile: any
@@ -12,7 +13,8 @@ export function ProfileInfo(props: ProfileInfoPropsType) {
     return <Preloader/>}
   return (   
         <div >
-       <img src="https://sun9-72.userapi.com/c854024/v854024783/139230/wZlmqJc56E4.jpg" />
+       <img src={mainImg}/>
+       {/* <img src="https://sun9-72.userapi.com/c854024/v854024783/139230/wZlmqJc56E4.jpg" /> */}
             <div className={style.ava}>
                 <img src={props.profile.photos.large} />
                 {/* <img src="https://images.pexels.com/photos/3473525/pexels-photo-3473525.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/> */}
